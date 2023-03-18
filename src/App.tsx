f9d58a3616850ab5093721e35e5b80a4866e520f
tavolafourcade <tavolafourcade@gmail.com>
2023-03-17 22:27:33 -0500

    [ UPDATE ] Import Topbar in the Application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
 
+import Topbar from './scenes/global/Topbar'
 import { ColorModeContext, useMode } from './theme'
 const App: React.FC = () => {
   const [theme, colorMode] = useMode()
@@ -8,7 +9,9 @@ const App: React.FC = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
-          <main className='content'></main>
+          <main className='content'>
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
