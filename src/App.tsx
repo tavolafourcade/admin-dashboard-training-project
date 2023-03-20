2aa7e85ab94d4874100071f08a1eb25311496236
tavolafourcade <tavolafourcade@gmail.com>
2023-03-19 23:06:01 -0500

    [ UPDATE ] General structure of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 
+// import Bar from './scenes/bar'
+// import Calendar from './scenes/calendar'
+// import Contacts from './scenes/contacts'
+import Dashboard from './scenes/dashboard'
+import Sidebar from './scenes/global/Sidebar'
+// import FAQ from './scenes/faq'
+// import Form from './scenes/form'
+// import Geography from './scenes/geography'
 import Topbar from './scenes/global/Topbar'
+// import Invoices from './scenes/invoices'
+// import Line from './scenes/line'
+// import Pie from './scenes/pie'
+// import Team from './scenes/team'
 import { ColorModeContext, useMode } from './theme'
+
 const App: React.FC = () => {
   const [theme, colorMode] = useMode()
   return (
@@ -9,8 +23,22 @@ const App: React.FC = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
+          <Sidebar />
           <main className='content'>
             <Topbar />
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              {/* <Route path='/team' element={<Team />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/line' element={<Line />} />
+              <Route path='/faq' element={<FAQ />} />
+              <Route path='/geography' element={<Geography />} />
+              <Route path='/calendar' element={<Calendar />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
