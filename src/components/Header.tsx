c78aacf9eca2ed6382f23e5873c38777aa3f54ed
tavolafourcade <tavolafourcade@gmail.com>
2023-03-21 21:42:29 -0500

    [ ADD ] Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { ReactElement } from 'react'
+import { Box, Typography, useTheme } from '@mui/material'
+
+import { tokens } from '../theme'
+
+interface HeaderProps {
+  title: string
+  subtitle: string
+}
+const Header = ({ title, subtitle }: HeaderProps): ReactElement => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+  return (
+    <Box mb='30px'>
+      <Typography variant='h2' color={colors.grey[100]} fontWeight='bold' sx={{ mb: '5px' }}>
+        {title}
+      </Typography>
+      <Typography variant='h5' color={colors.greenAccent[400]}>
+        {subtitle}
+      </Typography>
+    </Box>
+  )
+}
+
+export default Header
