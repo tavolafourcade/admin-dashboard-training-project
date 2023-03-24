4325216a88085ae1b3fdeab46c322168fa6eba57
tavolafourcade <tavolafourcade@gmail.com>
2023-03-24 10:59:03 -0500

    [ ADD ] Team section implementation

---
+++ src/scenes/team/index.tsx
@@ -0,0 +1,83 @@
+import { ReactElement } from 'react'
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined'
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined'
+import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined'
+import { Box, Typography, useTheme } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+
+import Header from '../../components/Header'
+import { mockDataTeam } from '../../data/mockData'
+import { tokens } from '../../theme'
+
+const Team = (): ReactElement => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+  console.log('colors', colors)
+  const columns = [
+    { field: 'id', headerName: 'ID' },
+    { field: 'name', headerName: 'Name', flex: 1, cellClassName: 'name-column--cell' },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+    },
+    {
+      field: 'phone',
+      headerName: 'Phone Number',
+      flex: 1,
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      flex: 1,
+    },
+    {
+      field: 'access',
+      headerName: 'Access level',
+      flex: 1,
+      renderCell: ({ row: { access } }: { row: { access: string } }) => {
+        return (
+          <Box
+            component='div'
+            width='60%'
+            m='0 auto'
+            p='5px'
+            display='flex'
+            justifyContent='center'
+            bgcolor={access === 'admin' ? colors.greenAccent[600] : colors.greenAccent[700]}
+            borderRadius='4px'
+          >
+            {access === 'admin' && <AdminPanelSettingsOutlinedIcon />}
+            {access === 'manager' && <SecurityOutlinedIcon />}
+            {access === 'user' && <LockOpenOutlinedIcon />}
+            <Typography color={colors.grey[100]} sx={{ ml: '5px' }}>
+              {access}
+            </Typography>
+          </Box>
+        )
+      },
+    },
+  ]
+
+  return (
+    <Box m='20px'>
+      <Header title='Team' subtitle='Managing the team members' />
+      <Box
+        m='40px 0 0 0'
+        height='75vh'
+        sx={{
+          '& .MuiDataGrid-root': { border: 'none' },
+          '& .MuiDataGrid-cell': { borderBottom: 'none' },
+          '& .name-column-cell': { color: colors.greenAccent[300] },
+          '& .MuiDataGrid-columnHeaders': { backgroundColor: colors.blueAccent[700], borderBottom: 'none' },
+          '& .MuiDataGrid-virtualScroller': { backgroundColor: colors.primary[400] },
+          '& .MuiDataGrid-footerContainer': { borderTop: 'none', backgroundColor: colors.blueAccent[700] },
+        }}
+      >
+        <DataGrid rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  )
+}
+
+export default Team
