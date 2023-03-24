155a157332564b8a0c4e19008f3248de0c48b0b9
tavolafourcade <tavolafourcade@gmail.com>
2023-03-24 13:08:59 -0500

    [ ADD ] Contacts component

---
+++ src/scenes/contacts/index.tsx
@@ -0,0 +1,72 @@
+import { ReactElement } from 'react'
+import { Box, useTheme } from '@mui/material'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid'
+
+import Header from '../../components/Header'
+import { mockDataContacts } from '../../data/mockData'
+import { tokens } from '../../theme'
+
+const Team = (): ReactElement => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+
+  const columns = [
+    { field: 'id', headerName: 'ID', flex: 0.5 },
+    { field: 'registrarId', headerName: 'Registrar ID' },
+
+    { field: 'name', headerName: 'Name', flex: 1, cellClassName: 'name-column--cell' },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+    },
+    {
+      field: 'phone',
+      headerName: 'Phone Number',
+      flex: 1,
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      flex: 1,
+    },
+    {
+      field: 'address',
+      headerName: 'Address',
+      flex: 1,
+    },
+    {
+      field: 'city',
+      headerName: 'City',
+      flex: 1,
+    },
+    {
+      field: 'zipCode',
+      headerName: 'ZipCode',
+      flex: 1,
+    },
+  ]
+  const openSideBar = true
+  return (
+    <Box m='20px' sx={{ width: `${openSideBar ? 'calc(100% - 270px)' : 'calc(100% - 80)'}` }}>
+      <Header title='CONTACTS' subtitle='List of Contacts for future reference' />
+      <Box
+        m='40px 0 0 0'
+        height='75vh'
+        sx={{
+          '& .MuiDataGrid-root': { border: 'none' },
+          '& .MuiDataGrid-cell': { borderBottom: 'none' },
+          '& .name-column-cell': { color: colors.greenAccent[300] },
+          '& .MuiDataGrid-columnHeaders': { backgroundColor: colors.blueAccent[700], borderBottom: 'none' },
+          '& .MuiDataGrid-virtualScroller': { backgroundColor: colors.primary[400] },
+          '& .MuiDataGrid-footerContainer': { borderTop: 'none', backgroundColor: colors.blueAccent[700] },
+          '& .MuiDataGrid-toolbarContainer .MuiButton-text': { color: colors.grey[100] },
+        }}
+      >
+        <DataGrid rows={mockDataContacts} columns={columns} components={{ Toolbar: GridToolbar }} />
+      </Box>
+    </Box>
+  )
+}
+
+export default Team
