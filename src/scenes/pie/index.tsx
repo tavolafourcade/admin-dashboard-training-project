aa3a2bac3622ae432e31986b6b4fb957ed273da5
tavolafourcade <tavolafourcade@gmail.com>
2023-03-25 20:52:54 -0500

    [ ADD ] Pie section

---
+++ src/scenes/pie/index.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from 'react'
+import { Box } from '@mui/material'
+
+import Header from '../../components/Header'
+import PieChart from '../../components/PieChart'
+
+const Pie = (): ReactElement => {
+  return (
+    <Box m='20px'>
+      <Header title='Pie Chart' subtitle='Simple Pie Chart' />
+      <Box height='75vh'>
+        <PieChart />
+      </Box>
+    </Box>
+  )
+}
+
+export default Pie
